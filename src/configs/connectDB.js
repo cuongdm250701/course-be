9e124f1694e4e338448783cce3aac514dea2a759
DESKTOP-F7V67DT\cuong <domanhcuong250701@gmail.com>
2021-12-29 00:35:26 +0700

    refactor: connectDB

--- src/configs/connectDB.js
+++ src/configs/connectDB.js
@@ -1,16 +1,21 @@
 const Sequelize = require("sequelize");
 require("dotenv").config();
 
-const sequelize = new Sequelize("course_it", "root", "manhcuong2001", {
-  host: "127.0.0.1",
-  port: process.env.DB_PORT,
-  dialect: "mysql",
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000, // Remove a connection from the pool after the connection has been idle (not been used) for 10 seconds
-  },
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: "mysql",
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000, // Remove a connection from the pool after the connection has been idle (not been used) for 10 seconds
+    },
+  }
+);
 
 module.exports = sequelize;
